c068568631c142849bc5e8c37e07d14bfe305038
Omar <omar0642624692@gmail.com>
2023-09-25 02:46:13 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,15 @@
 # Ensure SSH client is configured to use private key and refuse password authentication
 include stdlib
-
-file_line { 'Turn off passwd auth':
-  ensure => present,
-  path   => '/etc/ssh/sshd_config',
-  line   => 'PasswordAuthentication no',
-}
-
-file_line { 'Declare identity file':
+file_line { 'Refuse password authentication':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Set my identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
