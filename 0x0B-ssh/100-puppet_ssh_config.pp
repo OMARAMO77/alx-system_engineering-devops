b43e9faf3070f5297a813b15b00051cdf30fcfce
Omar <omar0642624692@gmail.com>
2023-09-25 02:48:27 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,13 +3,13 @@ include stdlib
 file_line { 'Refuse password authentication':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
+  line   => 'PasswordAuthentication no',
   replace => true,
 }
 
 file_line { 'Set my identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '     IdentityFile ~/.ssh/school',
+  line   => 'IdentityFile ~/.ssh/school',
   replace => true,
 }
