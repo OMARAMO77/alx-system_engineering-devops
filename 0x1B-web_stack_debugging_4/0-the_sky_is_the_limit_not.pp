70b78aca777e200a41af4250bdc3fd5aaed24a49
Omar <omar0642624692@gmail.com>
2023-11-16 04:04:00 -0800

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
 # Increase the amount of traffic "ULIMIT" an Nginx server can handle
-exec { 'upgrade ULIMIT':
+exec { 'upgrade-ULIMIT':
   path    => '/bin/',
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  command => 'sed -i "/ULIMIT/s/15/4096/" /etc/default/nginx',
 }
 
-exec { 'restart nginx':
+exec { 'restart-nginx':
   path    => '/bin/',
   command => 'service nginx restart',
 }
