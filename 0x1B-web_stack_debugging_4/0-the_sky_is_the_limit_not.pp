a7cf79aa64a91c96b9d8c99c6d78f48992f81464
Omar <omar0642624692@gmail.com>
2023-11-17 09:32:15 -0800

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,6 +4,7 @@ exec { 'upgrade-ULIMIT':
   command => 'sed -i "/ULIMIT/s/15/4096/" /etc/default/nginx',
 }
 
+# Restart Nginx
 exec { 'restart-nginx':
   path    => '/usr/sbin:/usr/bin:/sbin:/bin',
   command => 'service nginx restart',
